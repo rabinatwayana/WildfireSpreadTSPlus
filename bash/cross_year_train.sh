d9e06af55519bf8ef5f74ebabf1e60dbfbd25a4d
Twayana Rabina <e2406754@cluster-irisa.univ-ubs.fr>
2026-03-31 12:55:23 +0200

    complete cross-year-train 2018 random_split using doy temporal position

--- bash/cross_year_train.sh
+++ bash/cross_year_train.sh
@@ -1,17 +1,20 @@
+# ==================================================================
 # duration based stratified randon split for cross year experiment
+# ==================================================================
 ENTITY="wildfire_continual_learning"
 GROUP_NAME=cross_year_train
 LIGHTENING_LOG_DIR="./lightning_logs/cross_year_train"
 
-for YEAR in 2016 2017 2022 2023; do
-  RUN_NAME="utae_train_${YEAR}_dice"
+for YEAR in 2018; do
+  RUN_NAME="utae_train_${YEAR}_doy"
   PYTHONPATH="$PWD:$PWD/src" WANDB_MODE=online python src/train.py \
     -c cfgs/rabina/utae_all_features.yaml \
     --trainer cfgs/trainer_single_gpu.yaml \
     --data cfgs/data_multitemporal_full_features.yaml \
     --data.data_dir "/share/home/e2406754/cde_mt/CDE_Master_Thesis_Wildfire_CL/data/processed/WSTS_all_hdf5_optimized" \
     --trainer.max_epochs 100 \
-    --model.init_args.loss_function "Dice" \
+    --model.init_args.temporal_position_mode "doy" \
+    --data.return_doy true \
     --data.do_cross_year_experiment true \
     --data.cross_year_split_json_path "/share/home/e2406754/cde_mt/CDE_Master_Thesis_Wildfire_CL/output/data_split/cross_year_duration_stratified_split.json" \
     --data.cross_year_train_id "$YEAR" \
@@ -21,8 +24,12 @@ for YEAR in 2016 2017 2022 2023; do
     --trainer.logger.init_args.name "${RUN_NAME}"
 done
 
+# --model.init_args.loss_function "Dice" \
 
-# # spatial based for cross year experiment
+# ==================================================================
+# spatial based for cross year experiment
+# ==================================================================
+
 # ENTITY="wildfire_continual_learning"
 # GROUP_NAME=cross_year_train_spatial_split
 # LIGHTENING_LOG_DIR="./lightning_logs/cross_year_train_spatial_split"
